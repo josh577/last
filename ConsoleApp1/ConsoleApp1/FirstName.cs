591b0405aa8d9a32fb495022ea450cb7b322aeb9
Joshua Dodanduwa <10015281@student.toiohomai.ac.nz>
2018-08-02 09:29:04 +1200

    NZ done

---
+++ ConsoleApp1/ConsoleApp1/FirstName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person1
+    {
+        string fname;
+
+        public string Fname
+        {
+            get
+            {
+                return fname;
+            }
+
+            set
+            {
+                fname = value;
+            }
+        }
+
+
+        public Person1(string _fname)
+        {
+            Fname = _fname;
+        }
+    }
+
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person1 employee1 = new Person1("Josh");
+            Console.WriteLine(employee1.Fname);
+        }
+    }
+}
